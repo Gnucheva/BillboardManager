33a4b7f64b8197d8d520e880f96deadeaefd5ae4
Anastasia <n41111215@mail.ru>
2020-06-21 16:29:51 +0300

    Edited test

--- src/test/java/ru/netology/manager/MovieManagerTest.java
+++ src/test/java/ru/netology/manager/MovieManagerTest.java
@@ -21,21 +21,12 @@ class MovieManagerTest {
     private Movie ninth = new Movie(9, "NumberThree", "http://", "actionMovie");
     private Movie tenth = new Movie(10, "NumberFour", "http://", "actionMovie");
 
-    Movie[] expected = {new Movie(10, "NumberFour", "http://", "actionMovie"),
-            new Movie(9, "NumberThree", "http://", "actionMovie"),
-            new Movie(8, "NumberTwo", "http://", "actionMovie"),
-            new Movie(7, "NumberOne", "http://", "comedy"),
-            new Movie(6, "Trolls", "http://", "cartoon"),
-            new Movie(5, "InvisibleMan", "http://", "horrors"),
-            new Movie(4, "Gentlemen", "http://", "actionMovie"),
-            new Movie(3, "HotelBelgrad", "http://", "comedy"),
-            new Movie(2, "Ahead", "http://", "cartoon"),
-            new Movie(1, "Bladshot", "http://", "actionMovie")
+    Movie[] expected = {tenth, ninth, eighth, seventh, sixth, fifth, fourth, third, second, first};
 
-    };
 
-    @BeforeEach
-    public void setUp() {
+    @Test
+    void mustShowTenMovie() {
+        MovieManager manager = new MovieManager(10);
         manager.addMovie(first);
         manager.addMovie(second);
         manager.addMovie(third);
@@ -46,49 +37,61 @@ class MovieManagerTest {
         manager.addMovie(eighth);
         manager.addMovie(ninth);
         manager.addMovie(tenth);
-
+        Movie[] actual = manager.getLastAdd();
+        assertArrayEquals(expected, actual);
     }
 
     @Test
-    void mustShowTenMovie() {
-        MovieManager manager = new MovieManager(10);
-        manager.movies = this.manager.movies;
+    void mustShowNothing() {
+        MovieManager manager = new MovieManager(0);
+        manager.addMovie(first);
+        manager.addMovie(second);
+        manager.addMovie(third);
+        manager.addMovie(fourth);
+        manager.addMovie(fifth);
+        manager.addMovie(sixth);
+        manager.addMovie(seventh);
+        manager.addMovie(eighth);
+        manager.addMovie(ninth);
+        manager.addMovie(tenth);
         Movie[] actual = manager.getLastAdd();
         assertArrayEquals(expected, actual);
-
-
     }
 
     @Test
     void mustShowValid() {
         MovieManager manager = new MovieManager(5);
-        manager.movies = this.manager.movies;
+        manager.addMovie(first);
+        manager.addMovie(second);
+        manager.addMovie(third);
+        manager.addMovie(fourth);
+        manager.addMovie(fifth);
+        manager.addMovie(sixth);
+        manager.addMovie(seventh);
+        manager.addMovie(eighth);
+        manager.addMovie(ninth);
+        manager.addMovie(tenth);
         Movie movieToAdd = new Movie(11, "NumberFive", "http://", "horrors");
         manager.addMovie(movieToAdd);
         Movie[] actual = manager.getLastAdd();
-        Movie[] expected = {new Movie(11, "NumberFive", "http://", "horrors"),
-                new Movie(10, "NumberFour", "http://", "actionMovie"),
-                new Movie(9, "NumberThree", "http://", "actionMovie"),
-                new Movie(8, "NumberTwo", "http://", "actionMovie"),
-                new Movie(7, "NumberOne", "http://", "comedy")
-
-        };
+        Movie[] expected = {new Movie(11, "NumberFive", "http://", "horrors"), tenth, ninth, eighth, seventh};
         assertArrayEquals(expected, actual);
 
-    }
-
-    @Test
-    void mustShowZero() {
-        MovieManager manager = new MovieManager(0);
-        manager.movies = this.manager.movies;
-        Movie[] actual = manager.getLastAdd();
-        assertArrayEquals(expected, actual);
     }
 
     @Test
     void mustShowOverMax() {
         MovieManager manager = new MovieManager(11);
-        manager.movies = this.manager.movies;
+        manager.addMovie(first);
+        manager.addMovie(second);
+        manager.addMovie(third);
+        manager.addMovie(fourth);
+        manager.addMovie(fifth);
+        manager.addMovie(sixth);
+        manager.addMovie(seventh);
+        manager.addMovie(eighth);
+        manager.addMovie(ninth);
+        manager.addMovie(tenth);
         Movie[] actual = manager.getLastAdd();
         assertArrayEquals(expected, actual);
 

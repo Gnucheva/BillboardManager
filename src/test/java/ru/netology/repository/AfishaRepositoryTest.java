b2d4e5f926a1eb0c4b737f52f2dbfd3781d39436
Anastasia <n41111215@mail.ru>
2020-06-21 17:59:02 +0300

    Edited test

--- src/test/java/ru/netology/repository/AfishaRepositoryTest.java
+++ src/test/java/ru/netology/repository/AfishaRepositoryTest.java
@@ -8,30 +8,39 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class AfishaRepositoryTest {
     private AfishaRepository repository = new AfishaRepository();
-    private Movie movieToAdd = new Movie(11, "NumberFive", "http://", "horrors");
+
+    private Movie first = new Movie(1, "Bladshot", "http://", "actionMovie");
+    private Movie second = new Movie(2, "Ahead", "http://", "cartoon");
+    private Movie third = new Movie(3, "HotelBelgrad", "http://", "comedy");
+    private Movie fourth = new Movie(4, "Gentlemen", "http://", "actionMovie");
+    private Movie fifth = new Movie(5, "InvisibleMan", "http://", "horrors");
+    private Movie sixth = new Movie(6, "Trolls", "http://", "cartoon");
+    private Movie seventh = new Movie(7, "NumberOne", "http://", "comedy");
+    private Movie eighth = new Movie(8, "NumberTwo", "http://", "actionMovie");
+    private Movie ninth = new Movie(9, "NumberThree", "http://", "actionMovie");
+    private Movie tenth = new Movie(10, "NumberFour", "http://", "actionMovie");
 
     @BeforeEach
     void setUp() {
-        repository.save(movieToAdd);
+        repository.save(first);
+        repository.save(second);
+        repository.save(third);
+        repository.save(fourth);
+        repository.save(fifth);
+        repository.save(sixth);
+        repository.save(seventh);
+        repository.save(eighth);
+        repository.save(ninth);
+        repository.save(tenth);
+
     }
 
 
     @Test
     void mustFindAll() {
+        repository.findAll();
         Movie[] actual = repository.findAll();
-        Movie[] expected = {new Movie(1, "Bladshot", "http://", "actionMovie"),
-                new Movie(2, "Ahead", "http://", "cartoon"),
-                new Movie(3, "HotelBelgrad", "http://", "comedy"),
-                new Movie(4, "Gentlemen", "http://", "actionMovie"),
-                new Movie(5, "InvisibleMan", "http://", "horrors"),
-                new Movie(6, "Trolls", "http://", "cartoon"),
-                new Movie(7, "NumberOne", "http://", "comedy"),
-                new Movie(8, "NumberTwo", "http://", "actionMovie"),
-                new Movie(9, "NumberThree", "http://", "actionMovie"),
-                new Movie(10, "NumberFour", "http://", "actionMovie"),
-                new Movie(11, "NumberFive", "http://", "horrors")
-
-        };
+        Movie[] expected = new Movie[]{first, second, third, fourth, fifth, sixth, seventh, eighth, ninth, tenth,};
         assertArrayEquals(expected, actual);
 
     }
@@ -41,17 +50,7 @@ class AfishaRepositoryTest {
         Movie movieToAdd = new Movie(12, "NumberSix", "http://", "horrors");
         repository.save(movieToAdd);
         Movie[] actual = repository.findAll();
-        Movie[] expected = {new Movie(1, "Bladshot", "http://", "actionMovie"),
-                new Movie(2, "Ahead", "http://", "cartoon"),
-                new Movie(3, "HotelBelgrad", "http://", "comedy"),
-                new Movie(4, "Gentlemen", "http://", "actionMovie"),
-                new Movie(5, "InvisibleMan", "http://", "horrors"),
-                new Movie(6, "Trolls", "http://", "cartoon"),
-                new Movie(7, "NumberOne", "http://", "comedy"),
-                new Movie(8, "NumberTwo", "http://", "actionMovie"),
-                new Movie(9, "NumberThree", "http://", "actionMovie"),
-                new Movie(10, "NumberFour", "http://", "actionMovie"),
-                new Movie(11, "NumberFive", "http://", "horrors"),
+        Movie[] expected = {first, second, third, fourth, fifth, sixth, seventh, eighth, ninth, tenth,
                 new Movie(12, "NumberSix", "http://", "horrors")
 
         };
@@ -79,19 +78,18 @@ class AfishaRepositoryTest {
     void shouldRemoveById() {
         repository.removeById(5);
         Movie[] actual = repository.findAll();
-        Movie[] expected = {new Movie(1, "Bladshot", "http://", "actionMovie"),
-                new Movie(2, "Ahead", "http://", "cartoon"),
-                new Movie(3, "HotelBelgrad", "http://", "comedy"),
-                new Movie(4, "Gentlemen", "http://", "actionMovie"),
-                new Movie(6, "Trolls", "http://", "cartoon"),
-                new Movie(7, "NumberOne", "http://", "comedy"),
-                new Movie(8, "NumberTwo", "http://", "actionMovie"),
-                new Movie(9, "NumberThree", "http://", "actionMovie"),
-                new Movie(10, "NumberFour", "http://", "actionMovie"),
-                new Movie(11, "NumberFive", "http://", "horrors")
-
+        Movie[] expected = new Movie[]{first, second, third, fourth, sixth, seventh, eighth, ninth, tenth,
         };
         assertArrayEquals(expected, actual);
     }
+
+    @Test
+    void shouldRemoveAll() {
+        repository.removeAll();
+        Movie[] actual = repository.findAll();
+        Movie[] expected = new Movie[]{};
+        assertArrayEquals(expected, actual);
+    }
+
 
 }
